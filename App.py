81304965ac9151a545d7d41e3da5aff142905f4a
TransactionReview <154547671+TransactionReview@users.noreply.github.com>
2023-12-26 22:53:54 +0900

    Add files via upload

--- App.py
+++ App.py
@@ -52,7 +52,7 @@ def main():
         st.session_state.processComplete = True
         
     if 'messages' not in st.session_state:
-        st.session_state['message'] = [{"role": "assistant", 
+        st.session_state['messages'] = [{"role": "assistant", 
                                         "content" : "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!:service_dog:"}]
 
     for message in st.session_state.messages:
